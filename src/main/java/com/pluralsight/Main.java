23e1171f187c2f89e65dfd4be93e8aadaa3ad5af
pmvroque15 <pmvroque@gmail.com>
2026-04-22 10:01:31 -0400

    Added Fromatted date, Another formatted date, Date and time for my timezone and GMT

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,13 +1,31 @@
 package com.pluralsight;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
+
 public class Main {
-    static void main() {
-        DateTimeFormatter formatter;
+    public static void main(String[] args) {
+        DateTimeFormatter formatter, anotherFormatter;
         LocalDate date = LocalDate.now();
         LocalTime time = LocalTime.now();
+        LocalDateTime today = LocalDateTime.now();
+        ZonedDateTime GMTNow = ZonedDateTime.now(ZoneId.of("GMT"));
+
+        formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        anotherFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        String formattedDate = date.format(formatter);
+        String anotherFormattedDate = date.format(anotherFormatter);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd, yyyy");
+        String fmtFormatted = today.format(fmt);
+
+        DateTimeFormatter gmt = DateTimeFormatter.ofPattern("MM dd, yyyy HH:mm");
+        String GMTFormatted = GMTNow.format(gmt);
+
+        System.out.println("Formatted date: " + formattedDate);
+        System.out.println("Another Formatted date: " + anotherFormattedDate);
+        System.out.println("Date and Time in my timezone: " + date.getDayOfWeek() + " " + fmtFormatted);
+        System.out.println("Date and Time (GMT): " + today.getDayOfWeek() + ", " + GMTFormatted);
     }
 }
